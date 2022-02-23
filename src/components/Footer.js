66083182c1a71d96a06cf763b197722ce600b469
Kaustubh Trivedi <kaus12tri@gmail.com>
2022-02-23 17:47:03 +0530

    Added model-viewer and added the 3D model

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,79 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <div className=''>
-            <div className="container-fluid bg-dark text-light footer " style={{'borderTopRightRadius': '2rem'}}>
-                <div className="container py-5">
-                    <div className="row g-5">
-                        <div className="container col-lg- col-md-5">
-                            <h4 className="ff-secondary text-center text-primary fw-normal mb-4">Contact Us</h4>
-                            <p className="mb-2 text-center ">Trinity College of Engineering And Research, Saswad - Bopdev - Pune Rd, Yewalewadi, Pune - 411048</p>
-                            <p className="mb-2 text-center"><i className="fa fa-envelope me-3"></i>dsctcoer2021@gmail.com</p>
-                            <div className="d-flex pt-2" style={{width: "127px",margin:" auto", justifyContent: 'center'}}>
-                                <a className="btn btn-outline-light btn-social text-center" href=""><i className="fab fa-linkedin-in"></i></a>
-                                <a className="btn btn-outline-light btn-social text-center mx-2" href=""><i className="fab fa-instagram"></i></a>
-                                <a className="btn btn-outline-light btn-social text-center mx" href=""><i className="fab fa-discord"></i></a>
-                            </div>
-                        </div>   
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="copyright">
-                        <div className="row">
-                            <div className="container col-md-6 text-center mb-3 mb-md-0" >
-                                 <a className='text-center' href="#" style={{textDecoration : "none", color : "white" }}> &copy; www.dsctcoer.com</a> All Right Reserved.
-                            </div>
-                            <p className="text-center"><a href="#">Back to top</a></p>  
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="">
+      <div
+        className="container-fluid bg-dark text-light footer "
+        style={{ borderTopRightRadius: "2rem" }}
+      >
+        <div className="container py-5">
+          <div className="row g-5">
+            <div className="container col-lg- col-md-5">
+              <h4 className="ff-secondary text-center text-primary fw-normal mb-4">
+                Contact Us
+              </h4>
+              <p className="mb-2 text-center ">
+                Trinity College of Engineering And Research, Saswad - Bopdev -
+                Pune Rd, Yewalewadi, Pune - 411048
+              </p>
+              <p className="mb-2 text-center">
+                <i className="fa fa-envelope me-3"></i>dsctcoer2021@gmail.com
+              </p>
+              <div
+                className="d-flex pt-2"
+                style={{
+                  width: "127px",
+                  margin: " auto",
+                  justifyContent: "center",
+                }}
+              >
+                <a
+                  className="btn btn-outline-light btn-social text-center"
+                  href=""
+                >
+                  <i className="fab fa-linkedin-in"></i>
+                </a>
+                <a
+                  className="btn btn-outline-light btn-social text-center mx-2"
+                  href=""
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a
+                  className="btn btn-outline-light btn-social text-center mx"
+                  href=""
+                >
+                  <i className="fab fa-discord"></i>
+                </a>
+              </div>
             </div>
-            {/* <!-- Footer End --> */}
+          </div>
         </div>
-        
-    )
+        <div className="container">
+          <div className="copyright">
+            <div className="row">
+              <div className="container col-md-6 text-center mb-3 mb-md-0">
+                <a
+                  className="text-center"
+                  href="#"
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  {" "}
+                  &copy; www.dsctcoer.com
+                </a>{" "}
+                All Right Reserved.
+              </div>
+              <p className="text-center">
+                <a href="#">Back to top</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Footer End --> */}
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,84 +1,108 @@
-import React from 'react'
-import Footer from './Footer'
-import SubHome2 from './SubHome2'
-
+import React from "react";
+import Footer from "./Footer";
+import SubHome2 from "./SubHome2";
+import "@google/model-viewer";
+import Model from "../3D/dsc-custom-logo.gltf";
 
 const Home = () => {
-    return (
+  return (
+    <>
+      <div className="container flex space-between">
+        <img src="./images/imagebg.jpg" class="img-fluid" alt="..." />
+        <div>
+          <model-viewer
+            alt="Google Developers logo"
+            src={Model}
+            auto-rotate
+          ></model-viewer>
+        </div>
+      </div>
 
-        <>
-
-            <div className="container">
-                <img src="./images/imagebg.jpg" class="img-fluid" alt="..." />
-            </div>
-
-
-            <div class="container-xxl py-5">
-                <div class="container">
-                    <div className='border-bottom border-5 border-primary px-1' >
-
-                        <h5 class=" ff-secondary text-center text-primary fw-normal my-5" >Our Aim</h5>
-                    </div>
-                    <div class="row g-4 my-3">
-                        <div class="col-lg-3 col-sm-6 " >
-
-                            <div class="aim-item  pt-3" style={{boxShadow: "0 0 47px rgb(0 0 0 / 43%)", borderRadius: "19px"}}>
-                                <div class="p-4">
-                                    <i class="fas fa-users fa-2x"></i>
-                                    <h5>Connect</h5>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-3 col-sm-6" >
-                            <div class="aim-item pt-3" style={{boxShadow: "0 0 47px rgb(0 0 0 / 43%)", borderRadius: "19px"}}>
-                                <div class="p-4">
-                                    <i class="fas fa-lightbulb fa-2x"></i>
-                                    <h5>Learn</h5>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-3 col-sm-6  ">
-                            <div class="aim-item pt-3" style={{boxShadow: "0 0 47px rgb(0 0 0 / 43%)", borderRadius: "19px"}}>
-                                <div class="p-4">
-                                    <i class="fas fa-trophy fa-2x"></i>
-                                    <h5>Grow</h5>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div className='border-bottom border-5 border-primary px-1' >
-                            <h5 class=" ff-secondary text-center text-primary fw-normal my-5" >Helping Students Bridge The Gap Between Theory and Practice</h5>
-                        </div>
-
-                    </div>
+      <div class="container-xxl py-5">
+        <div class="container">
+          <div className="border-bottom border-5 border-primary px-1">
+            <h5 class=" ff-secondary text-center text-primary fw-normal my-5">
+              Our Aim
+            </h5>
+          </div>
+          <div class="row g-4 my-3">
+            <div class="col-lg-3 col-sm-6 ">
+              <div
+                class="aim-item  pt-3"
+                style={{
+                  boxShadow: "0 0 47px rgb(0 0 0 / 43%)",
+                  borderRadius: "19px",
+                }}
+              >
+                <div class="p-4">
+                  <i class="fas fa-users fa-2x"></i>
+                  <h5>Connect</h5>
                 </div>
+              </div>
             </div>
-
-
-
-            <div className="" >
-
-                <SubHome2 url="./images/connect.gif" para="Meet students interested in developer technologies at your university. All are welcome, including those with diverse backgrounds and different majors.
-" head = "Connect" />
-                <div style={{"background-color": "#d4d7da"}} >
-                <SubHome2 url="./images/Online-learning.gif" para="Learn about a range of technical topics and gain new skills through hands-on workshops, events, talks, and project-building activities - both online and in-person." head = "Learn" />
+            <div class="col-lg-3 col-sm-6">
+              <div
+                class="aim-item pt-3"
+                style={{
+                  boxShadow: "0 0 47px rgb(0 0 0 / 43%)",
+                  borderRadius: "19px",
+                }}
+              >
+                <div class="p-4">
+                  <i class="fas fa-lightbulb fa-2x"></i>
+                  <h5>Learn</h5>
                 </div>
-                <SubHome2 url="./images/growth2.gif" para="Apply new learnings to build great solutions for local problems. Advance your skills, career, and network. Give back to your community by helping others learn, too."  head = "Grow"/>
-
+              </div>
             </div>
 
+            <div class="col-lg-3 col-sm-6  ">
+              <div
+                class="aim-item pt-3"
+                style={{
+                  boxShadow: "0 0 47px rgb(0 0 0 / 43%)",
+                  borderRadius: "19px",
+                }}
+              >
+                <div class="p-4">
+                  <i class="fas fa-trophy fa-2x"></i>
+                  <h5>Grow</h5>
+                </div>
+              </div>
+            </div>
 
-            <Footer />
+            <div className="border-bottom border-5 border-primary px-1">
+              <h5 class=" ff-secondary text-center text-primary fw-normal my-5">
+                Helping Students Bridge The Gap Between Theory and Practice
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      <div className="">
+        <SubHome2
+          url="./images/connect.gif"
+          para="Meet students interested in developer technologies at your university. All are welcome, including those with diverse backgrounds and different majors.
+"
+          head="Connect"
+        />
+        <div style={{ "background-color": "#d4d7da" }}>
+          <SubHome2
+            url="./images/Online-learning.gif"
+            para="Learn about a range of technical topics and gain new skills through hands-on workshops, events, talks, and project-building activities - both online and in-person."
+            head="Learn"
+          />
+        </div>
+        <SubHome2
+          url="./images/growth2.gif"
+          para="Apply new learnings to build great solutions for local problems. Advance your skills, career, and network. Give back to your community by helping others learn, too."
+          head="Grow"
+        />
+      </div>
 
+      <Footer />
+    </>
+  );
+};
 
-        </>
-
-
-    )
-}
-
-export default Home
+export default Home;
